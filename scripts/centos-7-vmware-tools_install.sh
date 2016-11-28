38a4deab861d86abe30b20fd9675a4e09437bd5c
Stan Dorsett <stan.dorsett@gmail.com>
2016-11-28 21:48:30 +0000

    update scripts/centos-7-vmware-tools_install.sh to add symlink for vmtoolsd

--- scripts/centos-7-vmware-tools_install.sh
+++ scripts/centos-7-vmware-tools_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo yum upgrade ca-certificates -y
 sudo yum install perl bc open-vm-tools -y
+sudo ln -s /usr/bin/vmtoolsd /usr/sbin/vmtoolsd
 
 cd /tmp
 rm /tmp/vmware-tools-linux.iso
